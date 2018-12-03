8bbff97ac8f5ff9c14058e985f08498b5542493d
Boudewijn Meijer <bcm.meijer@gmail.com>
2018-12-03 13:44:27 +0100

    Optimized code

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <vector>
+#include <fstream>
 
 bool FindProcess(const char * exeName, DWORD& pid, std::vector<DWORD>& TheadIDs);
 
@@ -15,24 +16,23 @@ int main(int argc, const char * argv[]) {
 		HANDLE hProcessHandle = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, dwProcID);
 		std::cout << "[+] Opened Process\n";
 		if (hProcessHandle != NULL) {
-			wchar_t buffer[] = L"PUT DLL PATH HERE";
+			wchar_t buffer[] = L"C:\\Users\\Boudewijn\\Desktop\\theDLL64.dll";
 			auto p = VirtualAllocEx(hProcessHandle, nullptr, sizeof(buffer) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 			std::cout << "[+] Allocated Memory\n";
 			WriteProcessMemory(hProcessHandle, p, buffer, sizeof(buffer), nullptr);
 			std::cout << "[+] Written ProcMem\n";
 			std::cout << "[*] Looping through the threads and queueing APC...\n";
+			FARPROC LoadLibLocation = GetProcAddress(GetModuleHandle("kernel32"), "LoadLibraryW");
 			for (const auto& tid : TheadIDs) {
 				HANDLE hThread = OpenThread(THREAD_SET_CONTEXT, FALSE, tid);
 				if (hThread) {
-					int success = QueueUserAPC((PAPCFUNC)GetProcAddress(GetModuleHandle("kernel32"), "LoadLibraryW"), hThread, (ULONG_PTR)p);
-					if (success != 0) {
-					}
+					QueueUserAPC((PAPCFUNC)LoadLibLocation, hThread, (ULONG_PTR)p);
 				}
 				CloseHandle(hThread);
 			}
 			VirtualFreeEx(hProcessHandle, p, 0, MEM_RELEASE | MEM_DECOMMIT);
 		}
-		std::cout << "Cleaning up...\n";
+		std::cout << "[*] Cleaning up...\n";
 		CloseHandle(hProcessHandle);
 	}
 }
